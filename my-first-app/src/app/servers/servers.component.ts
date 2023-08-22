f4ce1e0dd992ea9c1a822375954e6b95f144b195
Ricardo Giaviti <rgiaviti@gmail.com>
2023-08-21 21:06:56 -0300

    Advacing

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,17 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]'
-  // selector: 'app-servers'
-  selector: '.app-servers',
+  selector: 'app-servers',
+  //selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was created!";
+  serverName: string = "Test";
 
   constructor() {
-
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }

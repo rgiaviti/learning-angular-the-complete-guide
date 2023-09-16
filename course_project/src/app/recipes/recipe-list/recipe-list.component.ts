28a2a652449344fe57db293d8b7cd1a7894e3b3a
Ricardo Giaviti <rgiaviti@gmail.com>
2023-09-15 21:30:19 -0300

    Another Lesson

--- course_project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course_project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://www.cookipedia.co.uk/wiki/images/b/b6/Chicken_a_la_King_recipe.jpg'),
+    new Recipe('Carbonara', 'Macarrão a Carbonara', 'https://assets.bonappetit.com/photos/5a6f48f94f860a026c60fd71/1:1/w_5111,h_5111,c_limit/pasta-carbonara.jpg')
+  ];
 
+  constructor() {}
+
+  ngOnInit() {
+    
+  }
 }
--- course_project/src/app/shopping-list/shopping-list.component.ts
+++ course_project/src/app/shopping-list/shopping-list.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients = [];
 
+  constructor() {
+
+  }
+
+  ngOnINit() {
+    
+  }
 }
